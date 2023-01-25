a3aba0fc04aec0fdf1e933f62740d4b33528c916
Nick <nfoote@utas.edu.au>
2023-01-26 08:00:59 +1100

    reduce logo size slightly. Remove unused logos

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -1,6 +1,6 @@
 import siteMetadata from '@/data/siteMetadata'
 import headerNavLinks from '@/data/headerNavLinks'
-import Logo from '@/data/logo3.svg'
+import Logo from '@/data/logo.svg'
 
 import Link from './Link'
 import SectionContainer from './SectionContainer'
