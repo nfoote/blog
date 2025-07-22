1120530de1ce2355917da127f37e433ac1fffdea
Nick Foote <n.foote09@gmail.com>
2025-07-22 14:51:18 +1000

    Run prettier over solution to tidy up files changed during package upgrades

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -22,28 +22,28 @@ const siteMetadata = require('../data/siteMetadata')
     <?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
       ${pages
-    .map((page) => {
-      if (page.includes('.md') && fs.existsSync(page)) {
-        const source = fs.readFileSync(page, 'utf8')
-        const fm = matter(source)
-        if (fm.data.draft || fm.data.canonicalUrl) return ''
-      }
+        .map((page) => {
+          if (page.includes('.md') && fs.existsSync(page)) {
+            const source = fs.readFileSync(page, 'utf8')
+            const fm = matter(source)
+            if (fm.data.draft || fm.data.canonicalUrl) return ''
+          }
 
-      const path = page
-        .replace('pages/', '/')
-        .replace('data/blog', '/blog')
-        .replace('public/', '/')
-        .replace(/(\.js|\.tsx|\.mdx|\.md)$/, '')
-        .replace('/feed.xml', '')
+          const path = page
+            .replace('pages/', '/')
+            .replace('data/blog', '/blog')
+            .replace('public/', '/')
+            .replace(/(\.js|\.tsx|\.mdx|\.md)$/, '')
+            .replace('/feed.xml', '')
 
-      if (page.includes('pages/404.') || page.includes('pages/blog/[...slug].')) return ''
+          if (page.includes('pages/404.') || page.includes('pages/blog/[...slug].')) return ''
 
-      return `
+          return `
             <url>
               <loc>${siteMetadata.siteUrl}${path === '/index' ? '' : path}</loc>
             </url>`
-    })
-    .join('')}
+        })
+        .join('')}
     </urlset>
   `
 
